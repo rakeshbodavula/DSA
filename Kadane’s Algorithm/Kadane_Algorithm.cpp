7427aa08864f645e5b1bf8ff1d8c79b814b1dadc
Rakesh <rakeshbodavula2002@gmail.com>
2021-10-12 15:30:50 +0530

    Kadane’s Algorithm

--- Kadane’s Algorithm/Kadane_Algorithm.cpp
+++ Kadane’s Algorithm/Kadane_Algorithm.cpp
@@ -1,3 +1,28 @@
+/*
+    Kadane's Algorithm
+
+Tags: 
+        1) DP
+        2) Greedy
+
+
+
+    Initialisation:
+        maximum=INT_MIN  so that if only negative numbers are present they can't be any error
+        sum=0
+    
+    Algo:
+        1) Traverse each element in the array
+        2) Each time add the element to the sum variable
+        3) And check if it is greater than the max sum till time
+        4) If greater assign that to maximum variable
+        5) If the sum is less that 0 then make it 0.
+
+Time Complexity: **O(n)**
+Space Complexity: **O(1)**
+
+*/
+
 #include<iostream>
 using namespace std;
 
